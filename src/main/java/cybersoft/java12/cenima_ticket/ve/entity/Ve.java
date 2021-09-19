be46565911608b28bcfa55059e73bf2c6e9e50d8
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2021-09-19 23:56:16 +0700

    add ve-phim relationship

--- src/main/java/cybersoft/java12/cenima_ticket/ve/entity/Ve.java
+++ src/main/java/cybersoft/java12/cenima_ticket/ve/entity/Ve.java
@@ -1,20 +1,28 @@
 package cybersoft.java12.cenima_ticket.ve.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import cybersoft.java12.cenima_ticket.common.entity.BaseEntity;
+import cybersoft.java12.cenima_ticket.film.entity.Phim;
 import cybersoft.java12.cenima_ticket.nguoi_dung.entity.NguoiDung;
 
 @Entity
 @Table(name = "eticket_ve")
 public class Ve extends BaseEntity {
+	@NotNull
+	private String maVe;
 	private int giaVe;
 	private String soGhe;
-	
+	@ManyToOne
+	@JoinColumn(name="thuocPhim")
+	private Phim thuocPhim;
 	@Column(name = "IdNguoiDung", insertable = false, updatable = false)
 	private int idNguoiDung;
 	
