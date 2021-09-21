ced809e5c88b3895255b48db3eb1895b192ac0ed
HuyNguyenBeosU <huy23042016@gmail.com>
2021-09-21 10:38:20 +0700

    quan he one-to-one giua ve va ghe

--- src/main/java/cybersoft/java12/cenima_ticket/ve/entity/Ve.java
+++ src/main/java/cybersoft/java12/cenima_ticket/ve/entity/Ve.java
@@ -2,17 +2,23 @@ package cybersoft.java12.cenima_ticket.ve.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import cybersoft.java12.cenima_ticket.Rap.entity.Ghe;
 import cybersoft.java12.cenima_ticket.common.entity.BaseEntity;
 import cybersoft.java12.cenima_ticket.nguoi_dung.entity.NguoiDung;
 
 @Entity
 @Table(name = "eticket_ve")
 public class Ve extends BaseEntity {
+	@Column(name = "giaVe")
 	private int giaVe;
+	
+	@Column(name = "soGhe")
 	private String soGhe;
 	
 	@Column(name = "IdNguoiDung", insertable = false, updatable = false)
@@ -21,4 +27,7 @@ public class Ve extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name = "IdNguoiDung")
 	private NguoiDung nguoiDung;
+	
+	@OneToOne(mappedBy = "ve",fetch = FetchType.LAZY)
+	private Ghe ghe;
 }
